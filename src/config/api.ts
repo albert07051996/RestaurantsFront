d55f75cf5d644bea4ef2292373633a9ca9b62b24
unami <albert.mirianashvili@gtu.ge>
2026-02-10 02:10:23 +0400

    comit

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://localhost:61015/api';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://restaurantsback.onrender.com/api';
+//'https://localhost:61015/api';
 
 export const apiClient = axios.create({ baseURL: API_BASE_URL });
 
